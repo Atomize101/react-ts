7c6c8fb4266f021b19a1ce5b2d2d2cce517226e3
Chris Patrick <atomize101@gmail.com>
2021-03-04 13:03:59 -0500

    Explicit Component Type Annotations

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,7 +1,12 @@
 interface ChildProps {
-    color: string;
+	color: string;
 }
 
-export const Child = ({color}: ChildProps) {
-    return <div>{color}</div>;
-}
+export const Child = ({ color }: ChildProps) => {
+	return <div>{color}</div>;
+};
+
+// The style below shows that the function is a react component
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+	return <div>{color}</div>;
+};
